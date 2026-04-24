5180906cf79a4dcf24aa31285b95fb6afd71fe1a
Valery Chumpitaz <valerychumpitaz2@gmail.com>
2026-04-24 08:54:52 -0500

    Update enrollments.component.ts

--- src/app/pages/enrollments/enrollments.component.ts
+++ src/app/pages/enrollments/enrollments.component.ts
@@ -1,14 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { Enrollment, EnrollmentService } from '../../services/enrollments.service';
- 
+
 @Component({
   selector: 'app-enrollments',
-    standalone: false,
+  standalone: false,
   styleUrl: './enrollments.component.css',
 
   templateUrl: './enrollments.component.html'
 })
 export class EnrollmentsComponent implements OnInit {
+  enrollments: Enrollment[] = [];
+  enrollment: Enrollment = { name: '', course: '', date: '' };
+  editMode = false;
 
-  
+
+  constructor(private service: EnrollmentService) { }
+
+
+  ngOnInit() {
+    this.load();
+  }
+
+
+  load() {
+    this.service.getAll().subscribe(data => this.enrollments = data);
+  }
+
+
+  save() {
+    if (this.editMode) {
+      this.service.update(this.enrollment.id!, this.enrollment)
+        .subscribe(() => this.load());
+      this.editMode = false;
+    } else {
+      this.service.create(this.enrollment)
+        .subscribe(() => this.load());
+    }
+
+
+    this.enrollment = { name: '', course: '', date: '' };
+  }
+
+
+  edit(e: Enrollment) {
+    this.enrollment = { ...e };
+    this.editMode = true;
+  }
+
+
+  delete(id: number) {
+    this.service.delete(id)
+      .subscribe(() => this.load());
+ }
 }
